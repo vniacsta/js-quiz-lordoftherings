8b90bbc8d0d983bece93695e63c9e0e13340b94e
Vania Costa <vniacsta@users.noreply.github.com>
2020-10-12 14:50:32 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+    question: 'When Frodo and the other hobbits first meet Aragorn, he goes by what name?',
+    answers: ['Arathorn', 'Ranger', 'Elessar', 'Strider'],
+    correct: 3
+  },
+  {
+    question: 'To open the gate to Moria, Gandalf says the Elvish word for:',
+    answers: ['Enter', 'Dwarf', 'Friend', 'Open'],
+    correct: 2
+  },
+  {
+    question: 'At the beginning of The Fellowship of the Ring, Bilbo is celebrating his birthday. How old is he?',
+    answers: [77, 101, 111, 135],
+    correct: 2
+  },
+  {
+    question: 'In what country was The Lord of the Rings trilogy filmed?',
+    answers: ['Australia', 'New Zealand', 'Norway', 'England'],
+    correct: 1
+  },
+  {
+    question: 'Whose voice is the first we hear in The Lord of the Rings?',
+    answers: ['Gandalf', 'Galadriel', 'Bilbo', 'Sauron'],
+    correct: 1
+  },
+  {
+    question: 'Aragorn is the heir of which king of Gondor?',
+    answers: ['Elendil', 'Isildor', 'Numendil', 'Arathorn'],
+    correct: 1
+  },
+  {
+    question: 'How are Merry and Pippin related?',
+    answers: ['Step brothers', 'Brothers', 'Not related', 'First cousins'],
+    correct: 3
+  },
+  {
+    question: 'Who directed the film Return of the King?',
+    answers: ['Guillermo Del Toro', 'Ridley Scott', 'Ron Howard', 'Peter Jackson'],
+    correct: 3
+  },
+  {
+    question: 'Including the Witch King, how many Nazgûl are there?',
+    answers: [9, 5, 8, 6],
+    correct: 0
+  }, 
+  {
+    question: 'What is the name of the volcano where the One Ring is forged and later destroyed?',
+    answers: ['Mount Morgoth', 'Mount Doom', 'Mount Death', 'Mount Mordor'],
+    correct: 1
+  }
+]
